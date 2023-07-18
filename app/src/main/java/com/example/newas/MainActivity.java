8fa9a293a85c9b71e0fbf0b3a097c193100a6f34
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2023-07-18 18:27:29 +0300

    everything final ish

--- app/src/main/java/com/example/newas/MainActivity.java
+++ app/src/main/java/com/example/newas/MainActivity.java
@@ -16,6 +16,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.graphics.Paint;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
